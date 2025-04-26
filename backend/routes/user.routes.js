2c7d52ef9940129c11ace6614a0ceeb1540c7b2b
Shivansh Agrawal <Shivanshagrawal3007@gmail.com>
2025-04-26 18:01:25 +0530

    change user.routes.js

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { login, register } from '../controllers/user.controller.js';
-import { upload } from '../middlewares/multer.js';
+import  upload  from '../middlewares/multer.js';
 
 const router=express.Router();
 router.post('/register', upload.single('image'), register);
